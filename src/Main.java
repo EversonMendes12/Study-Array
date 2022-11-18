2ae21871b20fd143ab80441322a2aba64b3c5a98
PCRS <eversonmendes301@gmail.com>
2022-11-18 15:59:23 -0300

    Array

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+        public static void main(String[] args) {
+
+            /* ------------------------------------------------------------------------------------------- */
+
+        /* int[] tabela = new int[5]; // Cria uma Array de 5 posições, ex: [00], [00], [00], [00], [00];
+
+        tabela[2] = 2;
+
+        JOptionPane.showMessageDialog(null, "Posição: 0" + tabela[2]);
+
+        Imprime:: 02
+
+        */
+
+            /* ------------------------------------------------------------------------------------------- */
+
+            int[] tabela = {10, 5, 11, 8, 1, 24}; // Array predefinida ex: [10], [05], [11], [8], [01], [24];
+
+            for (int x = 0 ; x <= 5 ; x++){
+
+                System.out.println("Posição: [" + x + "] Número contido no Array: [" + tabela[x] + "]");
+
+            }
+
+        }
 }
